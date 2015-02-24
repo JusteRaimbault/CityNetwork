96a36857ca71723a12d96fda6dda36e142b3c79e
JusteRaimbault <raimbaultjwin@gmail.com>
2015-02-24 14:11:32 +0100

    working access Mendeley API

--- Models/Biblio/AlgoSR/algoSR.sh
+++ Models/Biblio/AlgoSR/algoSR.sh
@@ -3,7 +3,9 @@
 #  algoSR.sh
 #     $1 : basedir. Should contain credential, keywords, etc
 #
-#
+#  Requires :
+#     - jsawk
+#  
 #  Created by <a href="mailto:juste.raimbault@polytechnique.edu">Juste Raimbault</a> on 24/02/15.
 #  
 #
@@ -21,12 +23,29 @@ cd $1
 ##############
 
 # ¡ do not forget to put files in gitignore !
+# dangerous
 appID=`cat appID`
 appSecret=`cat appSecret`
 
 # request auth token
-curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -u $appID:$appSecret -d "grant_type=client_credentials&scope=all" https://api.mendeley.com/oauth/token
+accessToken=$(curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -u $appID:$appSecret -d "grant_type=client_credentials&scope=all" https://api.mendeley.com/oauth/token| jsawk 'return this.access_token')
+#echo $accessToken #OK
 
+# try a search request
+query='urban+form'
+
+curl 'https://api.mendeley.com/search/catalog?query='$query'&limit=10' \
+-H 'Authorization: Bearer '$accessToken \
+-H 'Accept: application/vnd.mendeley-document.1+json' \
+#| jsawk 'out(this.abstract+"\n\n\n");return null'
+
+
+
+
+
+#############
+## Try access Cortext ?
+#############
 
 
 
