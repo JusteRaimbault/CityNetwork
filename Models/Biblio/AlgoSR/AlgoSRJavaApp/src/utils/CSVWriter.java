0efe4bb75d76093ed2e2ab9f5fb50f55c14846e7
JusteRaimbault <raimbaultjwin@gmail.com>
2015-03-02 11:16:51 +0100

    csv writer

---
+++ Models/Biblio/AlgoSR/AlgoSRJavaApp/src/utils/CSVWriter.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package utils;
+
+import java.io.File;
+import java.io.FileWriter;
+
+/**
+ * @author Raimbault Juste <br/> <a href="mailto:juste.raimbault@polytechnique.edu">juste.raimbault@polytechnique.edu</a>
+ *
+ */
+public class CSVWriter {
+
+	public static void write(String filePath,String[][] data,String delimiter){
+		try{
+			FileWriter writer = new FileWriter(new File(filePath));
+			for(int i=0;i<data.length;i++){
+				//same row size requirement not checked
+				for(int j=0;j<data[i].length;j++){
+					writer.write(data[i][j]);
+					if(j!=(data[i].length-1)){writer.write(delimiter);}
+				}
+				if(i!=(data.length-1)){writer.write("\n");}
+			}
+			writer.close();
+		}catch(Exception e){e.printStackTrace();}
+		
+	}
+	
+}
