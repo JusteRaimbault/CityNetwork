c48ec4434b55308ea52d5b9b604ca1b578c48839
JusteRaimbault <raimbaultjwin@gmail.com>
2015-03-02 18:22:55 +0100

    test pull multi-repo

--- Models/Biblio/AlgoSR/stats.R
+++ Models/Biblio/AlgoSR/stats.R
@@ -39,13 +39,24 @@ executeAlgo(query,resDir,defNumIt,defKwLimit)
 query <- 'transportation+network+urban+growth'
 resDir <- 'Results/Biblio/AlgoSR/junk'
 limits<-c(2,5,10,15,20)
+maxIt <- 5
 
-nrefs = matrix(data=rep(0,20*length(limits)),nrow=20)
+
+nrefs = matrix(data=rep(0,maxIt*length(limits)),nrow=maxIt)
 for(l in 1:length(limits)){
-  res = executeAlgo(query,resDir,20,limits[l])
+  res = executeAlgo(query,resDir,maxIt,limits[l])
   for(k in 1:length(res[,1])){nrefs[k,l]=res[k,1]}
   for(k in length(res[,1]):20){nrefs[k,l]=nrefs[length(res[,1]),l]}
 }
+
+
+#########
+# Systematic explo in grid (some kws,limits)
+#    Q : what max limit ?
+#
+
+
+
 
 
 
