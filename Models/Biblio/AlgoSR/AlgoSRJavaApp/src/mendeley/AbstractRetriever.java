9c876fca12aae52ddcc3c74279a189dd09db8a18
JusteRaimbault <raimbaultjwin@gmail.com>
2015-03-31 19:39:26 +0200

    correction AbstractRetriever app

--- Models/Biblio/AlgoSR/AlgoSRJavaApp/src/mendeley/AbstractRetriever.java
+++ Models/Biblio/AlgoSR/AlgoSRJavaApp/src/mendeley/AbstractRetriever.java
@@ -17,11 +17,19 @@ public class AbstractRetriever {
 
 	/**
 	 * @param args
+	 * 
+	 * args[0] : title
+	 * args[1] : path to config file
+	 * 
 	 */
 	public static void main(String[] args) {
 		String title = args[0];
+		String confPath = args[1];
 		
 		// do not forget to setup api
+		
+		main.Main.setup(confPath);
+		
 		MendeleyAPI.setupAPI();
 		
 		// rq : replacement should not been needed as provided title will be already treated (in appscript ?)
