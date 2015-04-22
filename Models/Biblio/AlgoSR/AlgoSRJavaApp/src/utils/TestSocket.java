c778863f31a0046721f661b297225e6997b3c837
JusteRaimbault <raimbaultjwin@gmail.com>
2015-04-22 15:56:33 +0200

    running_ip_changing_requests_with_orchid

--- Models/Biblio/AlgoSR/AlgoSRJavaApp/src/utils/TestSocket.java
+++ Models/Biblio/AlgoSR/AlgoSRJavaApp/src/utils/TestSocket.java
@@ -4,6 +4,7 @@
 package utils;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
@@ -11,6 +12,8 @@ import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.URL;
 import java.net.URLConnection;
+
+import javax.net.SocketFactory;
 
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
@@ -26,20 +29,22 @@ import org.apache.http.protocol.HttpContext;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import com.subgraph.orchid.Tor;
+import com.subgraph.orchid.TorClient;
+import com.subgraph.orchid.sockets.OrchidSocketFactory;
+import com.subgraph.orchid.socks.Socks4Request;
+
 /**
  * @author Raimbault Juste <br/> <a href="mailto:juste.raimbault@polytechnique.edu">juste.raimbault@polytechnique.edu</a>
  *
  */
 public class TestSocket {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		
+	public static void test1(){
+
 		try{
-			//System.setProperty("socksProxyVersion","4");
-			SocketAddress addr = new InetSocketAddress("localhost", 9050);
+			//System.setProperty("socksProxyVersion","4A");
+			SocketAddress addr = new InetSocketAddress("localhost", 9052);
 			Proxy proxy = new Proxy(Proxy.Type.SOCKS, addr);
 			//System.out.println(proxy.type());
 			Socket socket = new Socket(proxy);
@@ -49,73 +54,154 @@ public class TestSocket {
 				// run a new tor at each loop ?
 				// --> test for run a new at each google blocking
 				// kill ols tor if exists
-				
-				
-	            Process p=Runtime.getRuntime().exec("/usr/local/bin/tor -f /Users/Juste/.torrc");
+				//Process p=null;
+				/*try
+		        {           
+					
+					
+		            Runtime rt = Runtime.getRuntime();
+		            System.out.println("Running new tor...");
+		            p=Runtime.getRuntime().exec("/usr/local/bin/tor -f /Users/Juste/.torrc");
+		            int exitVal = p.exitValue();
+		            System.out.println("Process exitValue: " + exitVal);
+		        } catch (Throwable t){t.printStackTrace();
+		          }
+	            */
 	            
-	            p.waitFor();
+	            /*p.waitFor();
 	            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
 	            String line=reader.readLine();
 
 	            while (line != null) {
 	                System.out.println(line);
 	                line = reader.readLine();
-	            }
-	            Thread.sleep(5000);
+	            }*/
+				
+				//System.out.println("sleep 10sec");
+	            //Thread.sleep(10000);
 	            
 				//URL url = new URL("https://scholar.google.com/scholar?cluster=14017146646478350844");
 				URL url = new URL("http://ipecho.net/plain");
 				URLConnection conn = url.openConnection(proxy);
-			
+				conn.setReadTimeout(10000);conn.setReadTimeout(10000);
 				// add header
 				//user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36
 				conn.setRequestProperty("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36");
 			
 			
-			//HttpClient client = new DefaultHttpClient();
+				//HttpClient client = new DefaultHttpClient();
 
-		    //context
-		    //HttpContext context = new BasicHttpContext();
-		    //add a cookie store to context
-		    //CookieStore cookieStore = new BasicCookieStore();
-			//context.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
+				//context
+				//HttpContext context = new BasicHttpContext();
+				//add a cookie store to context
+				//CookieStore cookieStore = new BasicCookieStore();
+				//context.setAttribute(ClientContext.COOKIE_STORE, cookieStore);
 			
-			//RequestConfig config = RequestConfig.custom().setProxy(new HttpHost("localhost",9050)).build();
-            //HttpGet request = new HttpGet("http://scholar.google.com/scholar?cluster=14017146646478350844");
-			//HttpGet request = new HttpGet("http://ipecho.net/plain");
-			//request.setConfig(config);
-			//HttpResponse response = client.execute(request);
+				//RequestConfig config = RequestConfig.custom().setProxy(new HttpHost("localhost",9050)).build();
+				//HttpGet request = new HttpGet("http://scholar.google.com/scholar?cluster=14017146646478350844");
+				//HttpGet request = new HttpGet("http://ipecho.net/plain");
+				//request.setConfig(config);
+				//HttpResponse response = client.execute(request);
 			
-			//System.out.println(System.getProperty("socksProxyVersion"));
+				//System.out.println(System.getProperty("socksProxyVersion"));
 			
-			//InetSocketAddress dest = new InetSocketAddress("ipecho.net/plain", 80);
-			//socket.connect(dest);
+				//InetSocketAddress dest = new InetSocketAddress("ipecho.net/plain", 80);
+				//socket.connect(dest);
 			
-			BufferedReader r = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String html="";
-			String currentLine=r.readLine();
-			while(currentLine!= null){html+=currentLine;currentLine=r.readLine();}
+				BufferedReader r = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				String html="";
+				String currentLine=r.readLine();
+				while(currentLine!= null){html+=currentLine;currentLine=r.readLine();}
 			
 			
-			Document dom = Jsoup.parse(html);
-			System.out.println(dom.getElementsByTag("body").first().html());
+				Document dom = Jsoup.parse(html);
+				System.out.println(dom.getElementsByTag("body").first().html());
 			
-			//Thread.sleep(30000);
+				//Thread.sleep(30000);
 			
-			//kill tor
-			p=Runtime.getRuntime().exec("/bin/cat /Users/Juste/.torpid;/bin/kill -9 `/bin/cat /Users/Juste/.torpid`");p.waitFor();
-			
-			 reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-             line=reader.readLine();
+				//kill tor
+				System.out.println("killing old tor");
+				//Process p=Runtime.getRuntime().exec("/bin/kill -9 `/bin/cat /Users/Juste/.torpid`");
+			/*
+				BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				String line=reader.readLine();
 
-            while (line != null) {    
-                System.out.println(line);
-                line = reader.readLine();
-            }
+				while (line != null) {    
+					System.out.println(line);
+					line = reader.readLine();
+				}
+			*/
+				//p.destroy();
 			
+				}
+			}catch(Exception e){e.printStackTrace();}
+	}
+	
+	
+	/**
+	 * Test for Orchid java client
+	 */
+	public static void testOrchid(){
+		
+		
+		for(int i=0;i<5;i++){
+		try{
+		//Socks4Request request = new Socks4Request(null, OrchidSocketFactory.getDefault().createSocket("localhost",9050));
+			//use the same client ? NO
+			TorClient torclient = new TorClient();
 			
-			}
+			torclient.start();
+			torclient.waitUntilReady();
+			torclient.enableSocksListener(9050);
+			//torclient.enableDashboard();
+			
+		
+		//Socket socket = torclient.getSocketFactory().createSocket("http://ipecho.net/plain", 80);
+		//System.out.println(socket.toString());
+		//socket.connect( new InetSocketAddress("localhost", 9050));
+		//InputStream in = socket.getInputStream();
+		//SocketFactory.getDefault().createSocket().bind( new InetSocketAddress("localhost", 9050));
+		SocketAddress addr = new InetSocketAddress("localhost", 9050);
+		Proxy proxy = new Proxy(Proxy.Type.SOCKS, addr);
+		Socket socket = new Socket(proxy);
+		URL url = new URL("http://ipecho.net/plain");
+		URLConnection conn = url.openConnection(proxy);
+		conn.setReadTimeout(30000);conn.setReadTimeout(30000);
+		conn.setRequestProperty("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36");
+		InputStream in = conn.getInputStream();
+		BufferedReader r = new BufferedReader(new InputStreamReader(in));
+		String html="";
+		String currentLine=r.readLine();
+		while(currentLine!= null){System.out.println(currentLine);html+=currentLine;currentLine=r.readLine();}
+		r.close();
+		//in.close();
+		//socket.close();
+		System.out.println("reader closed");
+		//Document dom = Jsoup.parse(html);
+		//System.out.println(dom.getElementsByTag("body").first().html());
+		//Thread.currentThread().stop();
+		//tor.getCircuitManager().openDirectoryCircuit().
+		
+		torclient.stop();
+		
 		}catch(Exception e){e.printStackTrace();}
+		finally{
+			//tor.stop();
+		}
+		}
+	}
+	
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// test1();
+		
+		testOrchid();
 		
 	}
 
