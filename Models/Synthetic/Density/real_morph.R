5a6a8dd8cfce342e018a30928d61840729ab8849
JusteRaimbault <raimbaultjwin@gmail.com>
2015-04-25 16:23:38 +0200

    computing_real_indics

--- Models/Synthetic/Density/real_morph.R
+++ Models/Synthetic/Density/real_morph.R
@@ -40,12 +40,35 @@ areasize <- 1000
 # extract a 100kmx100km area
 e <- extract(raw,extent(3000000,3100000,1000000,1100000))
 
+simplifyBlock<-function(data,resFactor,areasize){
+  m = matrix(data=data,nrow=areasize,byrow=TRUE)
+  m[is.na(m)] <- 0
+  res=matrix(0,areasize*resFactor,areasize*resFactor)
+  for(x in 1:(areasize*resFactor)){
+    for(y in 1:(areasize*resFactor)){
+      res[x,y]=sum(m[((x-1)/resFactor+1):(x/resFactor),((y-1)/resFactor+1):(y/resFactor)])
+    }
+  }
+  return(res)
+}
+
+vals = list()
+i=1
 for(x in seq(from=1,to=nrow(raw)-areasize,by=areasize)){
   for(y in seq(from=1,to=ncol(raw)-areasize,by=areasize)){
      show(paste(x,y))
      #e <- extract(raw,extent(x,x+areasize-1,y,y+areasize-1))
      e<-getValuesBlock(raw,row=x,nrows=areasize,col=y,ncols=areasize)
-     show(prod(is.na(e)))
+     # compute on areas with at least half not NA
+     if(sum(is.na(e))/length(e)<0.5){
+       show("computing indicators...")
+       #getting rasters
+       m=simplifyBlock(e,0.1,areasize)
+       r_pop = raster(m)
+       r_dens = raster(m/sum(m))
+       vals[[i]]=c(x,y,moranIndex(),averageDistance(),entropy(),rankSizeSlope())
+       i=i+1
+     }
   }
 }
 
