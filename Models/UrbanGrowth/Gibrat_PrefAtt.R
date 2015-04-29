1bf0c6dbdb6046b52ace3435b5564095ba1038ca
JusteRaimbault <raimbaultjwin@gmail.com>
2015-04-29 16:17:21 +0200

    latexisation_calculs_gibrat

---
+++ Models/UrbanGrowth/Gibrat_PrefAtt.R
@@ -0,0 +1,17 @@
+
+########################
+## Numerical Simulations to check link between Gibrat and Pref Att Models
+########################
+
+
+#########
+# returns matrix of gibrat trajectories
+# given :
+#   - final time t
+#   - 
+gibrat<- function(t,P0){
+  
+}
+
+
+
