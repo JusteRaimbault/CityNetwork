3852b0f5a7ecf95af65dde718405f1543efb196a
JusteRaimbault <raimbaultjwin@gmail.com>
2015-05-06 15:33:56 +0200

    num_verif_scaling_sensitivity

--- Models/Scaling/ScalingSensitivity.R
+++ Models/Scaling/ScalingSensitivity.R
@@ -7,6 +7,8 @@
 # numerical study and confirmation of analytical results
 #
 
+library(kernlab)
+library(plot3D)
 
 # Generate a random density distribution
 #   following a scaling law with params alpha>0, Pmax (P_i = Pmax*i^{-\alpha})
@@ -24,20 +26,33 @@ spatializedExpMixtureDensity <- function(gridSize,N,rmin,rmax,Pmax,alpha,tolThre
   # patches of the grid are 1 unit size (in r_min/max units)
   grid = matrix(0,gridSize,gridSize)
   
+  # matrix of coordinates
+  coords = matrix(c(c(matrix(rep(1:gridSize,gridSize),nrow=gridSize)),c(matrix(rep(1:gridSize,gridSize),nrow=gridSize,byrow=TRUE))),nrow=gridSize^2)
+  
   # first draw param distribs ? not needed
   # for exp distribs, P_i = 2pi*d_i*r_i^2
   #  -> take P from deterministic distrib ; draw r.
   
   for(i in 1:N){
+    show(i)
     pop_i = Pmax*i^{-alpha}
     r_i = runif(1,min=rmin,max=rmax)
     d_i = pop_i / (2*pi*(r_i^2))
     
     # find origin of that kernel
     #  -> one of points such that : d(bord) > rcut and \forall \vec{x}\in D(rcut),d(\vec{x})<tolThr.
+    pot = which(!pseudoClosing(grid>tolThreshold,r_i),arr.ind=TRUE)
+    show(length(pot))
+    if(length(pot)==0){
+      # Take a point with minimal density ?
+      pot = which(grid==min(grid),arr.ind=TRUE)
+    }
     
+    row = sample(nrow(pot),1)
+    center = matrix(pot[row,],nrow=1)
     
-    
+    # add kernel : use kernlab laplace kernel
+    grid = grid + (d_i * matrix(kernelMatrix(kernel=laplacedot(sigma=1/r_i),x=coords,y=center),nrow=gridSize))
     
   }
   
@@ -50,13 +65,56 @@ pseudoClosing <- function(mat,rcut){
     for(j in 1:ncol(mat)){
       if(i>rcut&&i<nrow(mat)-rcut+1&&j>rcut&&j<ncol(mat)-rcut+1){
         # dirty that way - should be quicker with convol.
-        if(sum(mat[i-rcut+1:i,j-rcut+1:j])>=1){res[i,j]=1}
+        # furthermore Manhattan distance, not best solution for distribs symmetric by rotation.
+        if(sum(mat[(i-rcut):(i+rcut),(j-rcut):(j+rcut)])>=1){res[i,j]=1}
       }else{res[i,j]=1}
     }
   }
   return(res)
 }
 
+
+
+# test the function
+test = spatializedExpMixtureDensity(400,20,10,10,200,0.5,0.01)
+persp3D(z=test)
+# -> not disgusting
+
+
+# test the scaling
+#
+# for counter proportionnal to density.
+
+# cut at threshold, get sizes of each cluster.
+
+# use connexAreas function of morphoAnalysis
+
+# function giving slope as a function of theta
+linFit <- function(d,theta){
+  c = connexAreas(d>theta)
+  pops = c()
+  for(i in 1:length(c)){pops=append(pops,sum(d[c[[i]]]))}
+  x=log(1:length(pops))
+  y=log(sort(pops,decreasing=TRUE))
+  return(lm(y~x,data.frame(x,y)))
+}
+
+#
+slope(test,0.02)
+persp3D(z=d)
+
+d=spatializedExpMixtureDensity(400,30,10,10,200,0.2,0.01)
+
+thetas=seq(from=0.01,to=0.1,by=0.005)
+slopes=c();rsquared=c()
+for(theta in thetas){
+  l=linFit(d,theta)
+  slopes=append(slopes,l$coefficients[2])
+  rsquared=append(rsquared,summary(l)$adj.r.squared)
+}
+
+plot(thetas,-slopes)
+plot(thetas,rsquared)
 
 
 
