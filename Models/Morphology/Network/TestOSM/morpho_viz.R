f81ecdea6da60108ce0c0695dc56af697dad20fa
JusteRaimbault <raimbaultjwin@gmail.com>
2015-06-29 22:26:13 +0200

    clustering_res

--- Models/Morphology/Network/TestOSM/morpho_viz.R
+++ Models/Morphology/Network/TestOSM/morpho_viz.R
@@ -2,6 +2,8 @@
 ## Analysis and Visualization of moephological density analysis
 
 library(RColorBrewer)
+library(ggplot2)
+library(MASS)
 
 real_raw = read.csv(
   #paste0(Sys.getenv("CN_HOME"),'/Results/Synthetic/Density/RealData/Numeric/france_20km_mar.-juin-09-23:46:42-2015.csv'),
@@ -22,9 +24,9 @@ for(j in no_outsiders_cols){
 real=real_raw[rows,]
 
 # renormalize
-for(j in 1:ncol(real_raw)){
-  real[,j]=(real[,j]-min(real[,j]))/(max(real[,j])-min(real[,j]))
-}
+#for(j in 1:ncol(real_raw)){
+#  real[,j]=(real[,j]-min(real[,j]))/(max(real[,j])-min(real[,j]))
+#}
 
 # indep measurements
 real_ind = real[5*(0:(nrow(real)/5))+1,]
@@ -34,13 +36,10 @@ real_ind = real[5*(0:(nrow(real)/5))+1,]
 # Check spatial distribution of indicators
 ###################
 
-indic="moran"
-plot(
-  real$y,1-real$x,
-  col= colorRampPalette(c( "darkgrey","yellow","red"))(10000)[cut(real[[indic]],10000,labels=FALSE)],
-  pch=16,cex=0.5,xaxt="n",yaxt="n",xlab="",ylab="",
-  main=indic,legend=TRUE
-)
+
+indic="max"
+p = ggplot(data.frame(x=real$y,y=1-real$x,density_max=real[[indic]]),aes(x=x,y=y,colour=density_max))
+p+geom_point()+xlab("")+ylab("")+labs(title=indic)+scale_colour_gradientn(colours=rev(rainbow(5)))+scale_y_continuous(breaks=NULL)+scale_x_continuous(breaks=NULL)
 
 # * use spatial smoothing ? (bord effect of mask square shape, 
 #     give square patterns for "thresholded" indicators, as max or moran)
@@ -51,9 +50,31 @@ plot(
 # Descriptive Statistics
 ###########
 
-plot(real[sample.int(nrow(real),size=1000),3:6],pch="+")
+plot(real[sample.int(nrow(real),size=2000),3:6],pch="+")
 plot(real[,3:6],pch="+")
 
+# hists
+indic="distance"
+hist(real[[indic]],breaks=1000,main="",xlab=indic,freq=FALSE)
+fit = coef(fitdistr(real[[indic]],"log-normal"))
+points((1:2000)/1000,dlnorm((1:2000)/1000,meanlog=fit[1],sdlog=fit[2]),type="l",col="red")
+
+
+
+###############
+# Clustering
+###############
+
+# with all data (incl pop and max)
+# test various cluster vals
+k=10;ccoef=c()
+for(k in 1:15){
+  show(k)
+  clust = kmeans(real[,3:9],k,iter.max=30)
+  #ccoef=append(ccoef,sum(clust$withinss/clust$size)/k)# mean cluster size
+  ccoef=append(ccoef,clust$tot.withinss/clust$betweenss)# clust coef
+  #plot(real$y,1-real$x,col=clust$cluster,pch='.',cex=3,main=paste0('k=',k),xlab="",ylab="",xaxt='n',yaxt='n')
+}
 
 
 
