70c9a3684f9d2c22fab64f2be4661947e38ee22f
JusteRaimbault <raimbaultjwin@gmail.com>
2015-09-30 20:34:39 +0200

    first_test_nltk

---
+++ Models/Biblio/TextProcessing/testNLTK.py
@@ -0,0 +1,30 @@
+
+## Test for text processing, stems extraction
+
+import nltk
+
+raw_text = open('data/corpus_example.txt').read()
+#print(type(raw_text))
+print('Raw Text : ',raw_text)
+
+# Tokenize
+
+tokens = nltk.word_tokenize(raw_text)
+print('Words : ',len(tokens))
+
+tokens_wordpunct = nltk.wordpunct_tokenize(raw_text)
+print('Wordpunct : ',len(tokens_wordpunct))
+
+text = nltk.Text(tokens)
+
+# POS tagging ?
+
+
+###########
+
+# test stemmers
+
+porter = nltk.PorterStemmer()
+lancaster = nltk.LancasterStemmer()
+#test the porter
+print([porter.stem(t) for t in tokens])
