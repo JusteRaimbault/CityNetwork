b40f9f1ec69ddd8b6a32248816fe558037dbaa0f
JusteRaimbault <raimbaultjwin@gmail.com>
2015-10-22 15:51:14 +0200

    test_osmar

--- Models/Morphology/Network/TestOSM/test_osm.R
+++ Models/Morphology/Network/TestOSM/test_osm.R
@@ -4,5 +4,27 @@ library(igraph)
 
 setwd(paste0(Sys.getenv('CN_HOME'),'/Models/Morphology/Network/TestOSM'))
 
+#
+api <- osmsource_api()
+# quite slow for large areas ; use localconnexion to osm file ?
+# -> using osmosis
+osmosis <- osmsource_osmosis(file = paste0(Sys.getenv('CN_HOME'),'Data//OSM/')
+
+
+# OLG : (2.3815,48.8265)
+# height,width of box are in meters ; coordinates as decimal geographical
+area <- center_bbox(2.3815,48.8265,500,500)
+
+data <- get_osm(area,source = api)
+
+# request
+hways <- find(data, way(tags(k == "highway")))
+
+names(data$ways)
+# -> use dataframe to get objects
+
+data$ways$attrs
+
+
 
 
