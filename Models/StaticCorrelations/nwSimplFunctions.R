ea7d5f14d229f4b2ed01d29cc408eda9c5a9adc6
JusteRaimbault <raimbaultjwin@gmail.com>
2016-02-26 19:55:24 +0100

    daily

--- Models/StaticCorrelations/nwSimplFunctions.R
+++ Models/StaticCorrelations/nwSimplFunctions.R
@@ -61,22 +61,39 @@ simplifyGraph<-function(g){
   degrees=degree(g)
   remvertices = V(g)[which(degrees==2)]
   edgestoadd=V(g)[0];vtodelete=V(g)[0]
+  edgespeed=c();edgelength=c();edgetype=c()
   while(length(remvertices)>0){
     v=remvertices[1]
     n=neighbors(g,v);prevo=v;prevd=v
     o=n[1];d=n[2];p=c(v,o,d)
+    elengths = c();espeeds = c();etypes=c();
+    
+    # TODO : - length when only v is deleted -
+    
     while(max(degree(g,v=o))==2){
+      # link o - prevo added to cumulate speed and distance
+      e=E(g) [ o %--% prevo];
+      elengths=append(elengths,spDistsN1(pts = matrix(c(prevo$x,prevo$y),nrow=1),pt = c(o$x,o$y),longlat = TRUE))
+      espeeds=append(espeeds,e$speed);etypes=append(etypes,e$type)
       no=neighbors(g,o);tmpo=o;o=no[which(no!=prevo)];prevo=tmpo;p=append(p,o)
-      # TODO link o - prevo added to cumulate weight
     }
-    while(max(degree(g,v=d))==2){nd=neighbors(g,d);tmpd=d;d=nd[which(nd!=prevd)];prevd=tmpd;p=append(p,d)}
+    while(max(degree(g,v=d))==2){
+      e=E(g) [ d %--% prevd];
+      elengths=append(elengths,spDistsN1(pts = matrix(c(prevd$x,prevd$y),nrow=1),pt = c(d$x,d$y),longlat = TRUE))
+      espeeds=append(espeeds,e$speed);etypes=append(etypes,e$type)
+      nd=neighbors(g,d);tmpd=d;d=nd[which(nd!=prevd)];prevd=tmpd;p=append(p,d)
+    }
     # delete path vertices and add edge
     remvertices=difference(remvertices,p[which(p!=o&p!=d)])
-    edgestoadd=append(edgestoadd,c(o,d))
+    edgestoadd=append(edgestoadd,c(o$name,d$name))
+    edgelength=append(edgelength,sum(elengths))
+    edgespeed=append(edgespeed,sum(elengths*as.numeric(espeeds))/sum(elengths))
+    type="NA";nt=0;for(tt in unique(etypes)){if(length(which(etypes==tt))>nt){type=tt}}
+    edgetype=append(edgetype,type)
     vtodelete=append(vtodelete,p[which(p!=o&p!=d)])
     show(length(remvertices))
   }
-  return(delete_vertices(g,vtodelete)%>% add_edges(edgestoadd))
+  return(list(graph = delete_vertices(g,vtodelete),edgestoadd=edgestoadd,edgelength=edgelength,edgespeed=edgespeed,edgetype=edgetype))
 }
 
 #gg<-add_edges(g,edgestoadd)
@@ -86,9 +103,44 @@ simplifyGraph<-function(g){
 #add_edges(gg,V(gg)[which(V(gg)$name %in% edgestoadd$name)
 
 
+insertEdgeQuery<-function(o,d,length,speed,type){
+  
+  # TODO : index to not duplicate (-> necessary ? )
+  
+  return(paste0(
+     "INSERT INTO links (origin,destination,length,speed,roadtype,geography) values (",
+     #"'",o$name,d$name,type,"',
+     o$name,",",d$name,",",length,",",speed,",'",type,"',",
+     "ST_GeographyFromText('LINESTRING(",o$x," ",o$y,",",d$x," ",d$y,")'));"
+    )
+  )
+}
+
 #'
-#'
-exportGraph<-function(g){
+#' 
+#' insertion into simplified database : insert into links (id,origin,destination,geography) values ('1',10,50,ST_GeographyFromText('LINESTRING(-122.33 47.606, 0.0 51.5)')); 
+exportGraph<-function(sg){
+  # get simpl base connection
+  con = dbConnect(dbDriver("PostgreSQL"), dbname="osm_simpl",user="Juste",host="localhost" )
+  
+  dbSendQuery(con,"BEGIN TRANSACTION;")
+  
+  # first insert graph edges
+  for(e in E(sg$graph)){
+    dbSendQuery(con,)
+  }
+  
+  graph=sg$graph
+  sg$edgespeed[which(is.nan(sg$edgespeed))]=0
+  sg$edgespeed[which(is.na(sg$edgespeed))]=0
+  # then supplementary edges
+  for(i in seq(from=1,to=length(sg$edgestoadd),by=2)){
+    o=V(graph)[[sg$edgestoadd[i]]];d=V(graph)[[sg$edgestoadd[i+1]]]
+    dbSendQuery(con,insertEdgeQuery(o,d,sg$edgelength[1+(i-1)/2],sg$edgespeed[1+(i-1)/2],sg$edgetype[1+(i-1)/2]))
+  }
+  
+  dbCommit(con)
+  dbDisconnect(con)
   
 }
 
