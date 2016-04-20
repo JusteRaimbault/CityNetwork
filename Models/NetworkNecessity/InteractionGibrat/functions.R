85992dbef7a760fccd0d3f9b06ec101e1160aa56
JusteRaimbault <raimbaultjwin@gmail.com>
2016-04-20 20:25:55 +0200

    comp_optim_intgib

--- Models/NetworkNecessity/InteractionGibrat/functions.R
+++ Models/NetworkNecessity/InteractionGibrat/functions.R
@@ -11,6 +11,25 @@ potentials <-function(populations,distances,gammaGravity,decayGravity){
 }
 
 
+gibratModel <- function(real_populations,growthRate){
+  populations = matrix(0,nrow(real_populations),ncol(real_populations))
+  populations[,1] = real_populations[,1]
+  pflat=real_populations[,1];
+  tflat=rep(1,nrow(real_populations));
+  cflat=1:nrow(real_populations);
+  realflat = real_populations[,1]
+  for(t in 2:ncol(real_populations)){
+    populations[,t] = populations[,t-1]*(1 + growthRate)
+    pflat=append(pflat, populations[,t]);tflat=append(tflat,rep(t,Ncities));cflat=append(cflat,1:Ncities)
+    realflat=append(realflat,real_populations[,t])
+  }
+  return(list(df=data.frame(populations = pflat,real_populations=realflat,times=tflat,cities=cflat),
+              populations=populations
+        )
+  )
+}
+
+
 
 interactionModel <- function(real_populations,distances,gammaGravity=0,decayGravity=1,growthRate=0,potentialWeight=1){
   populations = matrix(0,nrow(real_populations),ncol(real_populations))
--- Models/NetworkNecessity/InteractionGibrat/real.R
+++ Models/NetworkNecessity/InteractionGibrat/real.R
@@ -5,6 +5,7 @@
 library(dplyr)
 library(sp)
 library(GA)
+library(ggplot2)
 
 setwd(paste0(Sys.getenv('CN_HOME'),'/Models/NetworkNecessity/InteractionGibrat'))
 
@@ -55,11 +56,43 @@ for(t0 in seq(1,21,by=5)){
   current_dates = t0:(t0+10)
   show(current_dates)
   real_populations = as.matrix(cities[,current_dates+3])
-  f =function(params){
+  fint =function(params){
     pops=interactionModel(real_populations,distances,params[1],params[2],params[3],params[4])$df;
     return(-sum((pops$populations-pops$real_populations)^2))}
-  optim = ga(type="real-valued",fitness = f,min = c(0.75,25,0.01,0.5),max=c(2,500,0.1,5),maxiter = 50000,parallel = 10)
-  save(optim,file=paste0('res/fitTw_t0',t0,'.RData'))
+  optimint = ga(type="real-valued",fitness = fint,min = c(0.75,50,0.01,0.1),max=c(1.5,750,0.1,10),maxiter = 100000,parallel = 10)
+  
+  fgib = function(params){ 
+    pops=gibratModel(real_populations,params[1])$df
+    return(-sum((pops$populations-pops$real_populations)^2))}
+  optimgib = ga(type="real-valued",fitness = fgib,min = c(0.001),max=c(0.2),maxiter = 100000,parallel = 10)
+  
+  save(optimgib,file=paste0('res/fitTw_gib_t0',t0,'.RData'))
+  save(optimint,file=paste0('res/fitTw_int_t0',t0,'.RData'))
 }
+
+
+#############
+#############
+
+# res=list();allpops=list()
+# t0 = seq(1,21,by=5)
+# for(i in 1:length(t0)){
+#   current_dates = t0[i]:(t0[i]+10)
+#   load(paste0('res/fitTw_t0',t0[i],'.RData'))
+#   res[[i]] = optim
+#   show(optim@solution)
+#   real_populations = as.matrix(cities[,current_dates+3])
+#   pops=interactionModel(real_populations,distances,optim@solution[1],optim@solution[2],optim@solution[3],optim@solution[4])$df;
+#   allpops[[i]]=pops
+#   g=ggplot(pops)
+#   g+geom_point(aes(x=times,y=populations,colour=cities),shape=2)+geom_point(aes(x=times,y=gibrat_populations,colour=cities),shape=3)
+#   
+# }
+# 
+# g=ggplot(allpops[[5]])
+# g+geom_point(aes(x=times,y=populations,colour=cities),shape=2)+geom_point(aes(x=times,y=gibrat_populations,colour=cities),shape=3)+geom_line(aes(x=times,y=real_populations,colour=cities,group=cities))
+# 
+
+
 
 
