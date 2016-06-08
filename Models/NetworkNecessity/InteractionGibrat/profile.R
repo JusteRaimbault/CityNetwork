8aad754ef0dd1b260c0929be64bc377e5a870a00
JusteRaimbault <raimbaultjwin@gmail.com>
2016-06-08 20:28:31 +0200

    daily

--- Models/NetworkNecessity/InteractionGibrat/profile.R
+++ Models/NetworkNecessity/InteractionGibrat/profile.R
@@ -1,10 +1,10 @@
 
 # calib profiles
 
-setwd(paste0(Sys.getenv("CN_HOME"),'/Results/NetworkNecessity/InteractionGibrat/profile/nofeedback/20160607_mselog/data'))
+setwd(paste0(Sys.getenv("CN_HOME"),'/Results/NetworkNecessity/InteractionGibrat/profile/nofeedback/20160607_logmse/data'))
 
 
-periods=c("1881-1901","1962-1982")
+periods=c("1831-1851","1841-1861","1851-1872","1881-1901","1962-1982")
 params=c("growthRate","gravityWeight","gravityGamma","gravityDecay")
 lastgen=100
 
@@ -15,7 +15,7 @@ for(param in params){
     dd=read.csv(paste0(p,'/',param,'/population',lastgen,'.csv'))
     d=rbind(d,cbind(dd,period=rep(p,nrow(dd))))
   }
-  g=ggplot(d,aes_string(x=param,y="mselog"))
+  g=ggplot(d,aes_string(x=param,y="logmse"))
   plots[[param]]=g+geom_line()+facet_wrap(~period,scales = "free")
 }
 
