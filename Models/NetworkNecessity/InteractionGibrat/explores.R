43c8152017a62faa51f707b6e73d266ae6ed7e50
JusteRaimbault <raimbaultjwin@gmail.com>
2016-07-27 19:32:18 +0200

    reading_record

--- Models/NetworkNecessity/InteractionGibrat/explores.R
+++ Models/NetworkNecessity/InteractionGibrat/explores.R
@@ -6,7 +6,7 @@ library(ggplot2)
 
 setwd(paste0(Sys.getenv('CN_HOME'),'/Results/NetworkNecessity/InteractionGibrat/exploration/test'))
 
-res <- as.tbl(read.csv('2016_07_26_15_24_19_LHS_GRID_LOCAL.csv'))
+res <- as.tbl(read.csv('2016_07_27_12_10_32_GRID_NOFEEDBACK_LOCAL.csv'))
 
 #
 #m = lm(logmse~gravityDecay+gravityGamma+gravityWeight+growthRate,res)
@@ -14,8 +14,8 @@ res <- as.tbl(read.csv('2016_07_26_15_24_19_LHS_GRID_LOCAL.csv'))
 # d <- res %>% mutate(gfg=floor(feedbackGamma*6)/6,ggd=floor(gravityDecay/100)*100)
 # res%>%group_by(growthRate)%>%summarise(logmse=mean(logmse))
 d=res#[which(res$growthRate==0.007),]
-gp = ggplot(d,aes(x=gravityDecay,y=mselog,colour=gravityGamma))
-gp+geom_point()+facet_grid(growthRate~gravityWeight,scales="free_y")#+stat_smooth()
+gp = ggplot(d,aes(x=growthRate,y=mselog,colour=gravityGamma,group=gravityGamma))
+gp+geom_line()+facet_grid(gravityWeight~gravityDecay,scales="free")#+stat_smooth()
 
 
 ######
@@ -35,6 +35,33 @@ res$rate=res$feedbackWeight/res$growthRate
 p1="rate";p2="feedbackDecay";p3="feedbackGamma";p4="growthRate";err="logmse"
 g=ggplot(res)#[abs(res$growthRate-0.071)<0.0001,])#res[res$growthRate==0.07|res$growthRate==0.06,])
 g+geom_line(aes_string(x=p1,y=err,colour=p2,group=p2),alpha=0.7)#+facet_grid(paste0(p3,"~",p4),scales="free")#)))+stat_smooth()#+facet_wrap(~gravityGamma,scales = "free")
+
+
+
+
+
+
+####
+# Determination of range of min growth rates
+
+minlogmse = c();minmselog=c()
+for(gravityWeight in unique(res$gravityWeight)){
+  for(gravityGamma in unique(res$gravityGamma)){
+    for(gravityDecay in unique(res$gravityDecay)){
+      d = res[res$gravityWeight==gravityWeight&res$gravityGamma==gravityGamma&res$gravityDecay==gravityDecay,]
+      minlogmse = append(minlogmse,d$growthRate[d$logmse==min(d$logmse)])
+      minmselog = append(minmselog,d$growthRate[d$mselog==min(d$mselog)])
+    }
+  }
+}
+
+#
+# -> range(minlogmse) ; range(minmselog)
+#
+data.frame(res[res$logmse==min(res$logmse),])
+data.frame(res[res$mselog==min(res$mselog),])
+
+
 
 
 
