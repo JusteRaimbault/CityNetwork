31607e9ef5aec13028d089246715d1da6826347f
JusteRaimbault <raimbaultjwin@gmail.com>
2017-07-16 20:39:10 +0800

    rad_bis

--- Models/Synthetic/Density/radius.R
+++ Models/Synthetic/Density/radius.R
@@ -20,14 +20,14 @@ sim1dprefAttDiff<-function(x0,alpha,beta,growth,t,nd=1,timesample=0,random=T){
 }
 
 getRadius<-function(alpha,beta){
-  sim<-sim1dprefAttDiff(c(rep(0,10000),1,rep(0,10000)),alpha,beta,10,100000,timesample=20000)
+  sim<-sim1dprefAttDiff(c(rep(0,10000),1,rep(0,10000)),alpha,beta,10,100000,timesample=20000,random = F)
   vals = sim$p[sim$t==max(sim$t)&sim$x>10000]
   return(which(cumsum(vals)>0.99*sum(vals))[1])
 }
 
 params = data.frame()
-for(alpha in seq(from=0.5,to=2.5,by=0.1)){
-  for(logbeta in seq(from=-3,to=-0.5,by=0.2)){
+for(alpha in seq(from=0.4,to=1.5,by=0.025)){
+  for(logbeta in seq(from=-4,to=-0.5,by=0.1)){
     params=rbind(params,data.frame(alpha=alpha,beta=10^logbeta))
   }
 }
@@ -44,7 +44,38 @@ res <- foreach(i=1:nrow(params)) %dopar% {
    return(getRadius(alpha,beta))
 }
 
-save(res,file='res/radius.RData')
+save(res,file='res/radius_deterministic.RData')
+
+###
+# load('res/radius.RData')
+# library(ggplot2)
+# 
+# d = data.frame(params,radius=unlist(res))
+# g=ggplot(d,aes(x=log(alpha),y=log(radius),colour=beta,group=beta))
+# g+geom_line()
+# 
+# g=ggplot(d,aes(x=log(beta),y=log(radius),colour=alpha,group=alpha))
+# g+geom_line()
+# 
+# fit=data.frame()
+# for(alpha in unique(d$alpha)){
+#   reg = lm(lradius~lbeta,data.frame(lbeta=log(d$beta[d$alpha==alpha]),lradius=log(d$radius[d$alpha==alpha])))
+#   summary(reg)$adj.r.squared
+#   fit=rbind(fit,data.frame(k=reg$coefficients[1],ksd=summary(reg)$coefficients[1,2],p=reg$coefficients[2],psd=summary(reg)$coefficients[2,2],rsq=summary(reg)$adj.r.squared,alpha=alpha))
+# }
+# 
+# g=ggplot(fit,aes(x=alpha,y=k,ymin=k-ksd,ymax=k+ksd))
+# g+geom_line()+geom_errorbar()
+# 
+# g=ggplot(fit,aes(x=alpha,y=log(-(log(k)/k-log(fit$k[nrow(fit)])/fit$k[nrow(fit)]))))
+# g+geom_line()+stat_smooth(span=2)
+# 
+# g=ggplot(fit,aes(x=alpha,y=p))
+# g+geom_line()+geom_errorbar(ymin=fit$p-fit$psd,ymax=fit$p+fit$psd)+stat_smooth(span = 0.4)+ylim(c(0.469,0.5))
+# 
+# g=ggplot(fit,aes(x=alpha,y=rsq))
+# g+geom_line()
+# 
 
 
 
