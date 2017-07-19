a7dc59d25ab0582a2ba9f4dc84ee248bfe2b43f5
Juste Raimbault <juste.raimbault@polytechnique.edu>
2017-07-19 16:06:06 +0200

    loadmongo

--- Models/StaticCorrelations/loadNWMongo.py
+++ Models/StaticCorrelations/loadNWMongo.py
@@ -8,6 +8,7 @@ mongohost = open('mongohost').readlines()[0].replace('\n','')
 mongo = pymongo.MongoClient(mongohost)
 db = mongo[database]
 col = db['network']
+col.delete_many({})
 
 # read the json file
 with open(jsonfile) as df:
@@ -15,16 +16,20 @@ with open(jsonfile) as df:
 
 # reformat the data
 cldata=[]
+i=0
 for feature in data['features']:
+    if i%100000==0 : print(i)
     currentrec = {}
-    for prop in feature['properties'].names():
+    for prop in feature['properties'].keys():
         currentrec[prop] = feature['properties'][prop]
     currentrec['geometry']=feature['geometry']
-    cldata.append(currentrec)
+    if currentrec['ORIGIN']!=currentrec['DESTINATIO']:
+        cldata.append(currentrec)
+    i=i+1
 
 print("Inserting features...")
-col.insert_many(currentrec)
+col.insert_many(cldata)
 
 # create index
 print("Creating spatial index...")
-col.create_index({'geometry':'2dsphere'})
+col.create_index([('geometry',pymongo.GEOSPHERE)])
