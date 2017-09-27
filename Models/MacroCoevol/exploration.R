9ec0bc4e52b8c3bde8a448ec209e853d792ec3a3
Juste Raimbault <juste.raimbault@polytechnique.edu>
2017-09-27 14:13:34 +0200

    all_retours

--- Models/MacroCoevol/exploration.R
+++ Models/MacroCoevol/exploration.R
@@ -4,9 +4,13 @@ library(dplyr)
 library(ggplot2)
 library(reshape2)
 
+source(paste0(Sys.getenv('CN_HOME'),'/Models/Utils/R/plots.R'))
+
 setwd(paste0(Sys.getenv('CN_HOME'),'/Results/MacroCoevol/Exploration'))
 
-res <- as.tbl(read.csv('20170613_virtual/data/20170613_164244_grid_virtual_local.csv',stringsAsFactors = FALSE,header=F,skip = 1))
+res <- as.tbl(read.csv('20170926_GRID_VIRTUAL/data/20170926_102630_GRID_VIRTUAL.csv',stringsAsFactors = FALSE,header=F,skip = 1))
+resdir='20170926_GRID_VIRTUAL/';
+
 
 finalTime = 30
 samplingStep = 5
@@ -17,33 +21,52 @@ distcorrbins = 1:10
 
 timelyNames<-function(arraynames,samplingTimes){res=c();for(t in samplingTimes){res=append(res,paste0(arraynames,t))};return(res)}
 
+#accessibilityEntropies,accessibilityHierarchies,accessibilitySummaries,closenessEntropies,
+# closenessHierarchies,closenessSummaries,complexityAccessibility,complexityCloseness,complexityPop,
+# diversityAccessibility,diversityCloseness,diversityPop,feedbackDecay,feedbackGamma,feedbackWeight,
+# finalTime,gravityDecay,gravityGamma,gravityWeight,id,nwGmax,nwThreshold,populationEntropies,
+# populationHierarchies,populationSummaries,rankCorrAccessibility,rankCorrCloseness,rankCorrPop,
+# replication,rhoClosenessAccessibility,rhoDistClosenessAccessibility,rhoDistPopAccessibility,
+#rhoDistPopCloseness,rhoPopAccessibility,rhoPopCloseness,synthRankSize
+
 names(res)<-c(
-  timelyNames(c("accessibilityEntropies"),samplingTimes),timelyNames(c("accessibilityHierarchies_alpha","accessibilityHierarchies_rsquared"),samplingTimes),timelyNames(c("accessibilitySummaries_mean","accessibilitySummaries_median","accessibilitySummaries_sd"),samplingTimes),
-  timelyNames(c("closenessEntropies"),samplingTimes),timelyNames(c("closenessHierarchies_alpha","closenessHierarchies_rsquared"),samplingTimes),timelyNames(c("closenessSummaries_mean","closenessSummaries_median","closenessSummaries_sd"),samplingTimes),
-  "complexityAccessibility","complexityCloseness","diversityAccessibility","diversityCloseness","diversityPop",
+  timelyNames(c("accessibilityEntropies"),samplingTimes),
+  timelyNames(c("accessibilityHierarchies_alpha","accessibilityHierarchies_rsquared"),samplingTimes),
+  timelyNames(c("accessibilitySummaries_mean","accessibilitySummaries_median","accessibilitySummaries_sd"),samplingTimes),
+  timelyNames(c("closenessEntropies"),samplingTimes),
+  timelyNames(c("closenessHierarchies_alpha","closenessHierarchies_rsquared"),samplingTimes),
+  timelyNames(c("closenessSummaries_mean","closenessSummaries_median","closenessSummaries_sd"),samplingTimes),
+  "complexityAccessibility","complexityCloseness","complexityPop","diversityAccessibility","diversityCloseness","diversityPop",
   "feedbackDecay","feedbackGamma","feedbackWeight","finalTime","gravityDecay","gravityGamma","gravityWeight","id","nwGmax","nwThreshold",
-  timelyNames(c("populationEntropies"),samplingTimes),timelyNames(c("populationHierarchies_alpha","populationHierarchies_rsquared"),samplingTimes),timelyNames(c("populationSummaries_mean","populationSummaries_median","populationSummaries_sd"),samplingTimes),
+  timelyNames(c("populationEntropies"),samplingTimes),
+  timelyNames(c("populationHierarchies_alpha","populationHierarchies_rsquared"),samplingTimes),
+  timelyNames(c("populationSummaries_mean","populationSummaries_median","populationSummaries_sd"),samplingTimes),
   "rankCorrAccessibility","rankCorrCloseness","rankCorrPop","replication",
   paste0("rhoClosenessAccessibility_tau",lags),
-  paste0("rhoDistClosenessAccessibility",distcorrbins),paste0("rhoDistPopAccessibility",distcorrbins),paste0("rhoDistPopCloseness",distcorrbins),
-  paste0("rhoPopAccessibility_tau",lags),paste0("rhoPopCloseness_tau",lags),
+  paste0("rhoDistClosenessAccessibility",distcorrbins),
+  paste0("rhoDistPopAccessibility",distcorrbins),
+  paste0("rhoDistPopCloseness",distcorrbins),
+  paste0("rhoPopAccessibility_tau",lags),
+  paste0("rhoPopCloseness_tau",lags),
   "synthRankSize"
 )
 
 #params = c("synthRankSize","feedbackDecay","feedbackGamma","feedbackWeight","gravityDecay","gravityGamma","gravityWeight","nwGmax","nwThreshold")
 params = c("synthRankSize","gravityDecay","gravityGamma","gravityWeight","nwGmax","nwThreshold")
 
-resdir='20170613_virtual/'
+
+
 
 #
-#data.frame(res%>%group_by(id)%>%summarise(count=n()))
-
-
+sres = res%>%group_by(id)%>%summarise(count=n())
+summary(sres$count)
+np=1;for(param in params){np=np*length(unlist(unique(res[,param])))}
+#np*50 : ok misses 30 runs only.
 
 ##
 # evolution of hierarchy in time
 vars = c("accessibilityEntropies","accessibilityHierarchies_alpha","accessibilityHierarchies_rsquared","accessibilitySummaries_mean","accessibilitySummaries_median","accessibilitySummaries_sd",
-         "closenessEntropies","closenessHierarchies_alpha","closenessHierarchies_rsquared","closenessSummaries_mean","closenessSummaries_median","closenessSummaries_mean",
+         "closenessEntropies","closenessHierarchies_alpha","closenessHierarchies_rsquared","closenessSummaries_mean","closenessSummaries_median","closenessSummaries_sd",
          "populationEntropies","populationHierarchies_alpha","populationHierarchies_rsquared","populationSummaries_mean","populationSummaries_median","populationSummaries_sd"
          )
 
@@ -65,10 +88,9 @@ for(gravityWeight in unique(res$gravityWeight)){
     g=ggplot(timedata[timedata$synthRankSize==synthRankSize&timedata$nwGmax==nwGmax&timedata$gravityWeight==gravityWeight,],
              aes_string(x="time",y=var,color="nwThreshold",group="nwThreshold")
              )
-    g+geom_point()+geom_smooth()+facet_grid(gravityGamma~gravityDecay)
+    g+geom_point()+geom_smooth()+facet_grid(gravityGamma~gravityDecay)+ggtitle(paste0("gravityWeight=",gravityWeight))+stdtheme
     #ggsave(paste0(resdir,'indics/no-nw_',var,'_gravityWeight',gravityWeight,'.pdf'),width=30,height=20,units='cm')
     ggsave(paste0(resdir,'indics/',var,'_gravityWeight',gravityWeight,'.pdf'),width=30,height=20,units='cm')
-    
    } 
 }
 
@@ -101,7 +123,7 @@ for(gravityWeight in unique(res$gravityWeight)){
 g=ggplot(lagdata[lagdata$synthRankSize==synthRankSize&lagdata$nwGmax==nwGmax&lagdata$gravityWeight==gravityWeight&lagdata$nwThreshold==nwThreshold,],
          aes(x=tau,y=rho,color=var,group=var)
          )
-g+geom_point(pch='.')+geom_smooth()+facet_grid(gravityGamma~gravityDecay)
+g+geom_point(pch='.')+geom_smooth()+facet_grid(gravityGamma~gravityDecay)+ggtitle(paste0("gravityWeight=",gravityWeight," ; nwThreshold=",nwThreshold))+stdtheme
 ggsave(paste0(resdir,'laggedcorrs/laggedcorrs_gravityWeight',gravityWeight,'_nwThreshold',nwThreshold,'.pdf'),width=30,height=20,units='cm')
 
   }
