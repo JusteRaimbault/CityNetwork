f512fad60a6bc3fc584391662158abbcfe5cde9b
Juste Raimbault <juste.raimbault@polytechnique.edu>
2017-10-02 13:47:31 +0200

    res_nacrocoevol

--- Models/MacroCoevol/calibration.R
+++ Models/MacroCoevol/calibration.R
@@ -5,9 +5,9 @@ source(paste0(Sys.getenv('CN_HOME'),'/Models/Utils/R/plots.R'))
 
 setwd(paste0(Sys.getenv('CN_HOME'),'/Models/MacroCoEvol/MacroCoEvol/calibres'))
 
-periods = c("1831-1851")#,"1841-1861","1851-1872","1881-1901","1891-1911","1921-1936","1946-1968","1962-1982","1975-1999")
+periods = c("1831-1851","1841-1861","1851-1872","1881-1901","1891-1911","1921-1936","1946-1968","1962-1982","1975-1999")
 
-resdir = '20170815_calibperiod_nsga_abstractnw_2'
+resdir = '20170929_calibperiod_nsga_abstractnw_local/'
 
 params = c("growthRate","gravityGamma","gravityDecay","gravityWeight"#,"feedbackGamma","feedbackDecay","feedbackWeight")
             ,"nwThreshold","nwExponent","nwGmax")
@@ -15,7 +15,7 @@ params = c("growthRate","gravityGamma","gravityDecay","gravityWeight"#,"feedback
 figdir = paste0(Sys.getenv('CN_HOME'),'/Results/MacroCoEvol/Calibration/',resdir,'/');dir.create(figdir)
 
 
-plots=list()
+#plots=list()
 for(param in params){
   cperiods = c();cparam=c();logmsepop=c();logmsedist=c()
   for(period in periods){
@@ -27,9 +27,41 @@ for(param in params){
     cperiods=append(cperiods,rep(period,nrow(res)));cparam=append(cparam,res[[param]])
   }
   g=ggplot(data.frame(logmsepop=logmsepop,logmsedist=logmsedist,param=cparam,period=cperiods),aes_string(x="logmsepop",y="logmsedist",colour="param"))
-  plots[[param]]=g+geom_point()+scale_colour_gradient(low="blue",high="red",name=param)+facet_wrap(~period,scales = "free")
+  #plots[[param]]=g+geom_point()+scale_colour_gradient(low="blue",high="red",name=param)+facet_wrap(~period,scales = "free")+xlab("log MSE population")+ylab("log MSE distance")
+  g+geom_point()+scale_colour_gradient(low="blue",high="red",name=param)+facet_wrap(~period,scales = "free")+xlab("log MSE population")+ylab("log MSE distance")+stdtheme
+  ggsave(paste0(figdir,"pareto_",param,".pdf"),width=30,height=20,units='cm')
 }
-multiplot(plotlist = plots,cols=3)
+#multiplot(plotlist = plots,cols=3)
+
+
+
+
+#############
+## param variation in time
+
+getDate<-function(s){(as.integer(strsplit(s,"-")[[1]][1])+as.integer(strsplit(s,"-")[[1]][2]))/2}
+
+filtered=F # makes no sense to filter in full model.
+for(param in params){
+  decays=c();sdDecay=c();types=c();ctimes=c()
+  for(period in periods){
+    latestgen = max(as.integer(sapply(strsplit(sapply(strsplit(list.files(paste0(resdir,'/',period)),"population"),function(s){s[2]}),".csv"),function(s){s[1]})))
+    res <- as.tbl(read.csv(paste0(resdir,'/',period,'/population',latestgen,'.csv')))
+    if(filtered){res=res[which(res$nwThreshold<15&res$gravityDecay<150),]}#&res$gravityGamma<5),]}
+    show(max(res$nwThreshold))
+    decays = append(decays,mean(unlist(res[,param])));sdDecay = append(sdDecay,sd(unlist(res[,param])));types = append(types,"pareto")
+    decays = append(decays,unlist(res[which(res$logmsepop==min(res$logmsepop))[1],param]));sdDecay=append(sdDecay,0);types = append(types,"logmsepop")
+    decays = append(decays,unlist(res[which(res$logmsedist==min(res$logmsedist))[1],param]));sdDecay=append(sdDecay,0);types = append(types,"logmsedist")
+    ctimes = append(ctimes,rep(getDate(period),3))
+  }
+  g=ggplot(data.frame(decay=decays,sd=sdDecay,type=types,time=ctimes),aes(x=time,y=decay,colour=type,group=type))
+  g+geom_point()+geom_line()+
+    geom_errorbar(aes(ymin=decay-sd,ymax=decay+sd))+ylab(param)+stdtheme
+  ggsave(file=paste0(figdir,'param_',param,'_filt',as.numeric(filtered),'.png'),width=20,height=15,units='cm')
+}
+
+
+
 
 
 
