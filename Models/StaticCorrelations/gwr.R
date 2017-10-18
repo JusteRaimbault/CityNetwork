385ad1031652f4598b7e4cf37cb698d40640ef8b
Juste Raimbault <juste.raimbault@polytechnique.edu>
2017-10-18 15:33:19 +0200

    gwr_allpoints

--- Models/StaticCorrelations/gwr.R
+++ Models/StaticCorrelations/gwr.R
@@ -29,8 +29,9 @@ sdata = sdata[apply(sdata,1,function(r){ifelse(length(which(is.na(r)))>0,F,T)}),
 
 morphoindics = c("moran","distance","entropy","slope")
 networkIndics = c("meanBetweenness","meanCloseness","networkPerf","vcount")
-nbootstrap = 25
-#nbootstrap = 1
+indics = c(morphoindics,networkIndics)
+#nbootstrap = 25
+nbootstrap = 1
 models=c();
 for(morphoindic in morphoindics){
   currentmodels = getLinearModels(morphoindic,networkIndics,length(networkIndics))
@@ -41,70 +42,94 @@ for(networkIndic in networkIndics){
   models = append(models,rep(currentmodels,nbootstrap))
 }
 
+data=sdata
+points = SpatialPointsDataFrame(coords=data.frame(data[,c("lonmin","latmin")]),data.frame(data),match.ID=F,proj4string = countries@proj4string)
+coords = points@coords
+dmat = gw.dist(dp.locat = coords,coords)
+
 
 library(doParallel)
-cl <- makeCluster(25,outfile='loggwr')
+cl <- makeCluster(50,outfile='loggwr')
 registerDoParallel(cl)
 
 resgwr <- foreach(i=1:length(models)) %dopar% {
   library(GWmodel);library(sp);set.seed(i)
   currentmodel = models[i]
   show(paste0('Model ',i,' : ',currentmodel))
-  data=sdata[sample.int(nrow(sdata),size=5000,replace = F),]
+  #data=sdata[sample.int(nrow(sdata),size=5000,replace = F),]
   #data=sdata
-  points = SpatialPointsDataFrame(coords=data.frame(data[,c("lonmin","latmin")]),data.frame(data),match.ID=F,proj4string = countries@proj4string)
-  bw = bw.gwr(currentmodel,data=points,approach = 'AIC',adaptive = T)
+  #points = SpatialPointsDataFrame(coords=data.frame(data[,c("lonmin","latmin")]),data.frame(data),match.ID=F,proj4string = countries@proj4string)
+  bw = bw.gwr(currentmodel,data=points,dMat = dmat,approach = 'AIC',adaptive = T)
   gw = gwr.basic(currentmodel,data=points,bw=bw,adaptive = T)
   d=spDists(points,longlat = T)
   meandist = mean(apply(d,1,function(r){mean(sort(r[r>0])[1:bw])}))
   return(list(bw=bw,meandist=meandist,aic = gw$GW.diagnostic$AICc,r2=gw$GW.diagnostic$gwR2.adj,model=currentmodel,indic=strsplit(currentmodel,split='~')[[1]][1]))
 }
 
-save(resgwr,file='res/gwr_full.RData')
+#save(resgwr,file='res/gwr_full.RData')
+save(resgwr,file='res/gwr_allpoints.RData')
 
-
-###############
-## analysis
 # 
-# load('res/gwr.RData')
+# ###############
+# ## analysis
+# # 
+# load('res/gwr_full.RData')
 # 
-# d = data.frame(bw=sapply(resgwr,function(l){l$bw}),
+# d = as.tbl(data.frame(bw=sapply(resgwr,function(l){l$bw}),
 #                meandist=sapply(resgwr,function(l){l$meandist}),
 #                aic=sapply(resgwr,function(l){l$aic}),
 #                model=sapply(resgwr,function(l){l$model}),
 #                indic=sapply(resgwr,function(l){l$indic})
-#                )
+#                ))
 # 
-# sres = as.tbl(d)%>%group_by(model,indic)%>%summarise(dist=mean(meandist),bw=mean(bw),distsd=sd(meandist),aicsd=sd(aic),aic=mean(aic))
+# # distrib of aics across models for one indic
+# g=ggplot(d[d$indic=='slope',],aes(x=model,y=aic))
+# g+geom_boxplot()
 # 
-# bestmodels = sres%>%group_by(indic)%>%summarise(bestmodel=model[which(aic==min(aic))],dist=dist[which(aic==min(aic))],bw=bw[which(aic==min(aic))])
+# sres = d%>%group_by(model,indic)%>%summarise(dist=mean(meandist),bw=mean(bw),distsd=sd(meandist),aicsd=sd(aic),aic=mean(aic))
 # 
-# data=sdata
-# points = SpatialPointsDataFrame(coords=data.frame(data[,c("lonmin","latmin")]),data.frame(data),match.ID=F,proj4string = countries@proj4string)
+# # tentative with min over all bootstraps - wrong but to see what happens
+# #bestmodels =d%>%group_by(indic)%>%summarise(bestmodel=model[which(aic==min(aic))],dist=meandist[which(aic==min(aic))],bw=bw[which(aic==min(aic))])
 # 
+# bestmodels = sres%>%group_by(indic)%>%summarise(bestmodel=model[which(aic-aicsd==min(aic-aicsd))],dist=dist[which(aic-aicsd==min(aic-aicsd))],bw=bw[which(aic-aicsd==min(aic-aicsd))])
 # 
-# for(i in 1:nrow(bestmodels)){
-#   gw = gwr.basic(as.character(bestmodels$bestmodel)[i],data=points,bw=floor(bestmodels$bw[i]),adaptive = T)
-# 
+# # use akaike weights
+# for(indic in indics){
+# #dd = d[d$indic==indic,]
+# dd = sres[sres$indic==indic,]
+# dd$weight = exp(-(dd$aic-min(dd$aic))/2)/sum(exp(-(dd$aic-min(dd$aic))/2))
+# #show(dd[dd$weight>quantile(dd$weight,0.8),])
+# show(dd[dd$weight>0.01,])
+# #show(max(dd$weight))
 # }
-
-
-
-###
-## test : distance matrix
-
+# 
+# # 
 # data=sdata
 # points = SpatialPointsDataFrame(coords=data.frame(data[,c("lonmin","latmin")]),data.frame(data),match.ID=F,proj4string = countries@proj4string)
+#  
+#  
+# # for(i in 1:nrow(bestmodels)){
+# gw = gwr.basic(as.character(bestmodels$bestmodel)[i],data=points,bw=floor(bestmodels$bw[i]),adaptive = T)
+# # 
+# # }
 # 
-# lon = sort(unique(data$lonmin));lat=sort(unique(data$latmin))
-# lonsample = lon[seq(from=5,to=length(lon),by=10)];latsample=lat[seq(from=5,to=length(lat),by=10)]
-# calib = matrix(c(c(matrix(rep(lonsample,length(latsample)),nrow = length(latsample),byrow = T)),rep(latsample,length(lonsample))),ncol = 2,byrow = F)
 # 
-# dmat = gw.dist(points@coords,calib,longlat = T)
 # 
-# gw = gwr.basic(meanCloseness~distance+entropy,data=points,bw=271,adaptive = T,dMat = dmat)
+# ###
+# ## test : distance matrix
 # 
-
+# # data=sdata
+# # points = SpatialPointsDataFrame(coords=data.frame(data[,c("lonmin","latmin")]),data.frame(data),match.ID=F,proj4string = countries@proj4string)
+# # 
+# # lon = sort(unique(data$lonmin));lat=sort(unique(data$latmin))
+# # lonsample = lon[seq(from=5,to=length(lon),by=10)];latsample=lat[seq(from=5,to=length(lat),by=10)]
+# # calib = matrix(c(c(matrix(rep(lonsample,length(latsample)),nrow = length(latsample),byrow = T)),rep(latsample,length(lonsample))),ncol = 2,byrow = F)
+# # 
+# # dmat = gw.dist(points@coords,calib,longlat = T)
+# # 
+# # gw = gwr.basic(meanCloseness~distance+entropy,data=points,bw=271,adaptive = T,dMat = dmat)
+# # 
+# 
 
 
 
