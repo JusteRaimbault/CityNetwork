a2685c3744d513d79565d9834f5d2226bca4300b
Juste Raimbault <juste.raimbault@polytechnique.edu>
2017-10-18 17:36:12 +0200

    automated_fig_gen

---
+++ Docs/Memoire/Final/figures.sh
@@ -0,0 +1,45 @@
+
+###############
+## Figures
+
+
+# general
+FIGDIR=Figures/Final
+
+# quality
+JPGQUALITY=50
+PDFRESOLUTION=200
+
+# size parameters
+WIDTH=2000
+HORIZONTALPADDING=10
+VERTICALPADDING=10
+
+## Setup
+
+mkdir $FIGDIR
+
+
+###############
+## Chapitre 4
+
+###############
+## 4.2 : Spatio-temp causalities
+
+# fig:causalityregimes:exrdb
+FIGNAME=4.2.2-fig-causalityregimes-exrdb
+echo $FIGNAME
+convert Figures/CausalityRegimes/laggedcorrs_facetextreme.png -resize "$WIDTH"x -quality $JPGQUALITY $FIGDIR/laggedcorrs_facetextreme.jpg
+montage Figures/CausalityRegimes/ex_60_wdens0_wroad1_wcenter1_seed272727.png Figures/CausalityRegimes/ex_60_wdens1_wroad1_wcenter0_seed272727.png Figures/CausalityRegimes/ex_60_wdens1_wroad1_wcenter1_seed272727.png -tile 3x1 -geometry +"$HORIZONTALPADDING"+0 Figures/Final/fig-causalityregimes-exrdb_tmp.png
+convert Figures/Final/fig-causalityregimes-exrdb_tmp.png -resize "$WIDTH"x -quality $JPGQUALITY Figures/Final/fig-causalityregimes-exrdb_tmp.jpg
+rm $FIGDIR/fig-causalityregimes-exrdb_tmp.png
+montage Figures/Final/fig-causalityregimes-exrdb_tmp.jpg Figures/Final/laggedcorrs_facetextreme.jpg -tile 1x2 -geometry +0+"$HORIZONTALPADDING" -quality $JPGQUALITY $FIGDIR/"$FIGNAME".jpg
+rm $FIGDIR/fig-causalityregimes-exrdb_tmp.jpg
+rm $FIGDIR/laggedcorrs_facetextreme.jpg
+
+
+# fig:causalityregimes:clustering
+FIGNAME=4.2.2-fig-causalityregimes-clustering
+echo $FIGNAME
+#montage Figures/CausalityRegimes/ccoef-knum_valuesFALSE_theta05-3.pdf Figures/CausalityRegimes/dccoef-knum_valuesFALSEtheta05-3.pdf -tile 3x1 -geometry +"$HORIZONTALPADDING"+0 $FIGDIR/"$FIGNAME"_tmp.pdf
+convert -density $PDFRESOLUTION Figures/CausalityRegimes/ccoef-knum_valuesFALSE_theta05-3.pdf -resize "$WIDTH"x -quality $JPGQUALITY $FIGDIR/"$FIGNAME"_tmp.jpg
