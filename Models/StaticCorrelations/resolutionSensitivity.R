da1614b46e0b7714ae5d5cdd9d37023f8fdc7516
Juste Raimbault <juste.raimbault@polytechnique.edu>
2017-11-12 21:00:40 +0100

    daily

--- Models/StaticCorrelations/resolutionSensitivity.R
+++ Models/StaticCorrelations/resolutionSensitivity.R
@@ -3,6 +3,7 @@ setwd(paste0(Sys.getenv('CN_HOME'),'/Models/StaticCorrelations'))
 
 source('functions.R')
 source('mapFunctions.R')
+source(paste0(Sys.getenv('CN_HOME'),'/Models/Utils/R/plots.R'))
 
 library(Matrix)
 
@@ -11,11 +12,12 @@ countrycode="FR"
 areasize=200;offset=100;factor=0.5
 res1 = loadIndicatorData(paste0("res/europecoupled_areasize",areasize,"_offset",offset,"_factor",factor,"_temp.RData"))
 
-#areasize=100;offset=50;factor=0.5
+areasize=100;offset=50;factor=0.5
 #res2 = loadIndicatorData("res/res/europe_areasize100_offset50_factor0.5_20160824.csv")
-
-areasize=60;offset=30;factor=0.5
 res2 = loadIndicatorData(paste0("res/europecoupled_areasize",areasize,"_offset",offset,"_factor",factor,"_temp.RData"))
+
+#areasize=60;offset=30;factor=0.5
+#res2 = loadIndicatorData(paste0("res/europecoupled_areasize",areasize,"_offset",offset,"_factor",factor,"_temp.RData"))
 
 
 
@@ -32,39 +34,30 @@ datapoints = SpatialPoints(data.frame(res2[,c("lonmin","latmin")]),proj4string =
 selectedpoints = gContains(country,datapoints,byid = TRUE);sdata2 = res2[selectedpoints,]
 datapoints2=datapoints[selectedpoints,]
 
+#plot(datapoints1)
+#plot(datapoints2,pch=".",col='red',add=T)
+
 dmat = spDists(datapoints1@coords,datapoints2@coords,longlat = T)
 
-d0 = 50
-
-M = exp(-dmat/d0)
-W = Diagonal(x=1/rowSums(M))%*%Matrix(M)
-
+corrs=c();indics=c();d0s=c()
+for(d0 in seq(1,25,2)){
+  show(d0)
+  M = exp(-dmat/d0)
+  W = Diagonal(x=1/rowSums(M))%*%Matrix(M)
+  for(indic in names(sdata1)){
 # use the weight matrix to compute smoothed field
-
 # correlations make sense on intensive indices only
-
-X1 = matrix(data=sdata1$moran,nrow=nrow(W));X1[is.na(X1)]=0
-Y1 = t(W)%*%X1
-X2 = matrix(data=sdata2$moran,nrow=ncol(W));X2[is.na(X2)]=0
-Y2 = W%*%X2
-#cor(sdata1$moran,Y[,1])
-cor(Y1[,1],Y2[,1])
-
-map(c(3),'moran_corresp.png',20,18,c(1,1),sdata=data.frame(lonmin=sdata1$lonmin,latmin=sdata1$latmin,moran=((sdata1$moran+Y[,1])/2)^10))
-
-
-X = matrix(data=sdata2$slope,nrow=ncol(W));X[is.na(X)]=0
+X = matrix(data=sdata2[,indic],nrow=ncol(W));X[is.na(X)]=0
 Y = W%*%X
-cor(sdata1$rankSizeAlpha,Y[,1])
+corrs=append(corrs,cor(sdata1[,indic],Y[,1]))
+d0s=append(d0s,d0);indics=append(indics,indic)
 
-X = matrix(data=sdata2$entropy,nrow=ncol(W));X[is.na(X)]=0
-Y = W%*%X
-cor(sdata1$entropy,Y[,1])
+#map(c(3),'moran_corresp.png',20,18,c(1,1),sdata=data.frame(lonmin=sdata1$lonmin,latmin=sdata1$latmin,moran=((sdata1$moran+Y[,1])/2)^10))
+}
+}
 
-X = matrix(data=sdata2$distance,nrow=ncol(W));X[is.na(X)]=0
-Y = W%*%X
-cor(sdata1$averageDistance,Y[,1])
-
+g=ggplot(data.frame(corr=corrs,d0=d0s,indic=indics),aes(x=d0,y=corr,color=indic,group=indic))
+g+geom_point()+geom_line()+stdtheme
 
 
 
