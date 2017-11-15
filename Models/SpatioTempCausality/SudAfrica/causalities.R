cb64c371e3a29c823e3a24f6740c5a6df7f8983f
Juste Raimbault <juste.raimbault@polytechnique.edu>
2017-11-15 12:00:32 +0100

    intro_partir_I

--- Models/SpatioTempCausality/SudAfrica/causalities.R
+++ Models/SpatioTempCausality/SudAfrica/causalities.R
@@ -17,6 +17,8 @@ load('data/pops.RData')
 load('data/distmats.RData')
 
 years = c(1911,1921,1936,1951,1960,1970,1980,1991)
+#mean(years[3:length(years)] - years[1:(length(years)-2)])
+#23.16667
 
 ###########
 ## Lagged correlations
@@ -76,8 +78,8 @@ Tw=3;mode='time'
 g=ggplot(res[res$Tw==Tw&res$mode==mode,],aes(x=tau,y=rho,ymin=rhomin,ymax=rhomax,color=d0,group=d0))
 g+geom_point()+geom_line()+geom_errorbar()+facet_wrap(~span)+
   geom_vline(aes(xintercept=0),col='red',linetype=2)+geom_hline(aes(yintercept=0),col='red',linetype=2)+
-  xlab(expression(tau))+ylab(expression(rho[tau](Delta*P[i],Delta*T[i])))+scale_shape_manual(values = unique(res$d0), guide=guide_legend(nrow=5,ncol=2),name=expression(d[0]))+stdtheme+
-  theme(legend.position = c(0.85, 0.25),legend.text=element_text(size=20))
+  xlab(expression(tau))+ylab(expression(rho[tau](Delta*P[i],Delta*T[i])))+scale_color_continuous(name=expression(d[0]))+stdtheme+
+  theme(legend.position = c(0.85, 0.25),legend.text=element_text(size=20),legend.title = element_text(size=20))
 ggsave(paste0(resdir,'laggedCorrs_time_Tw',Tw,'.png'),width=30,height=20,units='cm')
 
 
