63011fc509cd69fc02549d10826ee8192c8529d9
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-01-22 17:48:19 +0100

    .

---
+++ Models/Reflexivity/citation.R
@@ -0,0 +1,143 @@
+
+# Citation Network Analysis
+
+setwd(paste0(Sys.getenv('CN_HOME'),'/Models/Reflexivity'))
+
+library(dplyr)
+library(igraph)
+
+source(paste0(Sys.getenv('CN_HOME'),'/Models/QuantEpistemo/HyperNetwork/HyperNetwork/functions.R'))
+
+# raw network
+edges <- read.csv('data/CityNetwork_20171127_links.csv',sep=";",header=F,colClasses = c('character','character'))
+nodes <- as.tbl(read.csv('data/CityNetwork_20171127.csv',sep=";",header=F,stringsAsFactors = F,colClasses = c('character','character','character')))
+
+names(nodes)<-c("title","id","year")
+nodes=nodes[nchar(nodes$id)>0,]
+
+elabels = unique(c(edges$V1,edges$V2))
+empty=rep("",length(which(!elabels%in%nodes$id)))
+nodes=rbind(nodes,data.frame(title=empty,id=elabels[!elabels%in%nodes$id],year=empty))#,abstract=empty,authors=empty))
+
+citation <- graph_from_data_frame(edges,vertices = nodes[,c(2,1,3)])#3:7)])
+citation = induced_subgraph(citation,which(components(citation)$membership==1))
+#V(raw)$primary = V(raw)$name%in%nodesprim$V2
+
+#primary = induced_subgraph(raw,which(V(raw)$primary==TRUE))
+#V(primary)$reduced_title = sapply(V(primary)$title,function(s){substr(s,1,20)})
+#V(primary)$cut_title = sapply(V(primary)$title,function(s){paste0(substr(s,1,floor(nchar(s)/2)),'\n',substr(s,floor(nchar(s)/2)+1,nchar(s)))})
+#V(primary)$authoryear = paste0(V(primary)$authors,V(primary)$year)
+
+#V(raw)$primtitle = ifelse(V(raw)$primary,paste0(V(primary)$authors,V(primary)$year),"")
+#V(raw)$reduced_primtitle = ifelse(V(raw)$primary,sapply(V(raw)$title,function(s){paste0(substr(s,1,25),'...')}),"")
+
+V(citation)$reduced_title = sapply(V(citation)$title,function(s){paste0(substr(s,1,30),"...")})
+V(citation)$reduced_title = ifelse(degree(citation)>50,V(citation)$reduced_title,rep("",vcount(citation)))
+#V(raw)$reduced_title=rep("",vcount(raw))
+
+
+citationcore = induced_subgraph(citation,which(degree(citation)>1))
+
+#V(rawcore)$title = rep("",vcount(rawcore))
+
+#write_graph(raw,file='EvolutiveUrbanTheory/data/citation.gml',format = 'gml')
+#write_graph(primary,file='EvolutiveUrbanTheory/data/primary.gml',format = 'gml')
+
+#write_graph(rawcore,file='EvolutiveUrbanTheory/data/primarycore.gml',format = 'gml')
+write_graph(citationcore,file='data/rawcore.gml',format = 'gml')
+
+ecount(citationcore)/(vcount(citationcore)*(vcount(citationcore)-1))
+
+# comparison values : from [Batagelj, 2003]
+vref = c(40,223,396,1059,1059,3084,3244,4470,6752,8843,8851)#,3774768)
+eref = c(60,657,1988,4922,4929,10416,31950,12731,54253,41609,25751)#,16522438)
+summary(eref / (vref*(vref-1)))
+
+##
+# analysis of raw
+
+mean(degree(citation))
+mean(degree(citation,mode = 'in'))
+mean(degree(citationcore,mode = 'in'))
+
+
+##
+#  analysis of rawcore
+
+set.seed(0)
+
+#A = as.matrix(as_adjacency_matrix(citationcore,sparse = T))
+A=as_adjacency_matrix(citationcore,sparse = T)
+M = A+t(A)
+undirected_rawcore = graph_from_adjacency_matrix(M,mode="undirected")
+
+# communities
+com = cluster_louvain(undirected_rawcore)
+
+
+
+directedmodularity(com$membership,A)
+
+# randomise links
+nreps = 100
+mods = c()
+for(i in 1:nreps){
+  show(i)
+  mods=append(mods,directedmodularity(com$membership,A[sample.int(nrow(A),nrow(A),replace = F),sample.int(ncol(A),ncol(A),replace = F)]))
+}
+
+show(paste0(mean(mods)," +- ",sd(mods)))
+# -> 260 sds, ultra significant
+
+
+######
+# content of communities
+#
+
+d=degree(citationcore,mode='in')
+for(c in unique(com$membership)){
+  if(sizes(com)[c] > 10){
+    show(paste0("Community ",c, " ; corpus prop ",length(which(com$membership==c))/vcount(undirected_rawcore)))
+    #show(paste0("Size ",length(which(com$membership==c))))
+    currentd=d[com$membership==c];dth=sort(currentd,decreasing = T)[10]
+    show(data.frame(titles=V(citationcore)$title[com$membership==c&d>dth],degree=d[com$membership==c&d>dth]))
+    #show(V(rawcore)$title[com$membership==c])
+  }
+}
+
+# -> OK deteministic communities ; no need to save. (multi-level this louvain, not random ?)
+
+# nom des communautes de citation
+citcomnames=list('28'='Chaos','64'='Economic Geography','62'='Urban Systems','67'='ABM',
+                 '68'='Datamining','49'='Networks','47'='Spatial Statistics',
+                 '34'='Fractals','37'='Powe Laws','52'='Evolutionnary Economic Geography',
+                 '21'='Quantitative Epistemology','23'='Spatial Urban Growth Models',
+                 '60'='Complexity','66'='LUTI','50'='Physics of Cities',
+                 '14'='SPatio-temporal data','40'='Biological Networks',
+                 '63'='Space Syntax/Procedural modeling','51'='VGI'
+                 )
+
+#V(citationcore)$citclass = unlist(sapply(as.character(com$membership),function(n){ifelse(n%in%names(citcomnames),unlist(citcomnames[n]),'NA')}))
+V(citationcore)$citmemb = com$membership
+
+save(citation,citationcore,citcomnames,com,undirected_rawcore,file='processed/citation.RData')
+# load('HyperNetwork/HyperNetwork/processed/citation.RData')
+
+## these communities are on the core ; for semantic shall we extend ?
+#  -> compare with full communities
+
+#A = as.matrix(as_adjacency_matrix(citation))
+#M = A+t(A)
+#undirected_citation = graph_from_adjacency_matrix(M,mode="undirected")
+
+# communities
+#com = cluster_louvain(undirected_citation)
+
+# -> 50 coms, mod 0.83 - seems quite ???
+
+
+
+
+
+
+
---
+++ Models/Reflexivity/interdisc.R
@@ -0,0 +1,167 @@
+
+
+# (following sensitivity)
+
+#sub<-extractSubGraphCommunities(semantic,kminopt,kmaxopt,freqminopt,freqmaxopt,ethopt)
+#coms=sub$com;gg=sub$gg
+
+
+load(paste0('processed/',mongobase,'_probas_',kwLimit,'_eth',eth_graph,'_nonfiltdico_kmin',kminopt,'_kmax',kmaxopt,'_freqmin',freqminopt,'_freqmax',freqmaxopt,'_eth',ethopt))
+
+# content of communities
+
+for(k in unique(coms$membership)){
+  show(paste0('Com ',k,', size : ',length(which(coms$membership==k)),' , weight docs : ',100*colSums(probas)[k]/sum(probas)))
+   vertices=(coms$membership==k)
+   currentnames=V(gg)$name[vertices];currentdegree=degree(gg)[vertices]
+   degth = sort(currentdegree,decreasing = T)[min(20,length(currentdegree))]
+   show(data.frame(name=currentnames[currentdegree>degth],degree=currentdegree[currentdegree>degth]))
+}
+
+#semnames <- c('Maritime Networks','Accessibility','Sustainable Transport','Socio-economic','Policy',
+#              'Remote Sensing','Measuring','Agent-based Modeling','French Geography','Climate Change',
+#              'Environment','High Speed Rail','Transportation planning','Traffic','Health Geography',
+#              'Spanish Geography','Freight and Logistics','Mobility Data Mining','Education','Networks'
+#             )
+
+semnames <- c('brt','tod','hedonic','infra planning','hsr','complex networks','networks')
+
+
+# export network
+write_graph(gg,file=paste0(figdir,'subgraph.gml'),format = 'gml')
+
+# save(gg,coms,probas,semantic,keyword_dico,file='processed/semantic.RData')
+
+####
+rownames(probas)<-names(keyword_dico)
+colnames(probas)<-semnames
+
+# extract citation graph with probas
+
+subcit = induced_subgraph(citationcore,which(V(citationcore)$name%in%rownames(probas)[rowSums(probas)>0]))
+#subprobas = probas[V(subcit)$name,]
+subprobas = probas
+
+# interdisciplinarity
+interdisc = data.frame(interdisc = 1 - apply(subprobas^2,1,sum),id=as.character(rownames(subprobas)))#,citclass =  unlist(sapply(as.character(V(subcit)$citmemb),function(n){ifelse(n%in%names(citcomnames),unlist(citcomnames[n]),'NA')})))
+
+g=ggplot(interdisc[interdisc$citclass!='NA',],aes(x=interdisc,colour=citclass))
+g+geom_density(alpha=0.3)+stdtheme+xlab('interdisciplinarity')+scale_color_discrete(name='Cit. Class')
+ggsave(file=paste0(figdir,'interdisciplinarities.png'),width=20,height=10,units = 'cm')
+
+
+# composition of citation coms
+#dd=as.tbl(data.frame(content=c(subprobas),semclass=c(matrix(rep(semnames,nrow(subprobas)),byrow = F)),citclass=rep(as.character(interdisc$citclass),ncol(subprobas))))
+#compos = dd%>%group_by(citclass,semclass)%>%summarise(compo=sum(content))
+# why u no work ? # do it by hand ? -> OK FUCKING Factor
+selectedsem = c('Networks','Policy','Socio-economic','High Speed Rail','Education','Climate Change','Remote Sensing','Sustainable Transport')
+
+compos=c();cit=c();sem=c()
+for(citclass in citcomnames){
+  compos=append(compos,colSums(subprobas[interdisc$citclass==citclass,selectedsem]))
+  cit=append(cit,rep(citclass,length(selectedsem)));sem=append(sem,selectedsem)
+}
+
+dd=data.frame(citclass=cit,semclass=sem,compo=compos)
+
+g=ggplot(dd,aes(x=citclass,y=compo,fill=semclass))
+g+geom_col()+xlab('Citation class')+ylab('Composition')+scale_colour_discrete(name='Semantic')#+stdtheme
+ggsave(file=paste0(figdir,'compos.png'),width=20,height=10,units = 'cm')
+
+# same with proportions
+for(citclass in unique(dd$citclass)){dd$compo[dd$citclass==citclass]=dd$compo[dd$citclass==citclass]/sum(dd$compo[dd$citclass==citclass])}
+g=ggplot(dd,aes(x=citclass,y=compo,fill=semclass))
+g+geom_col()+xlab('Citation class')+ylab('Proportion')+scale_colour_discrete(name='Semantic')#+stdtheme
+ggsave(file=paste0(figdir,'compo_proportion.png'),width=20,height=10,units = 'cm')
+
+
+###
+# Proximities between citation communities
+
+# citation proximities
+
+proxmat = matrix(0,length(citcomnames),length(citcomnames))
+adj=A[rownames(subprobas),rownames(subprobas)]
+for(i in 1:length(citcomnames)){
+  for(j in 1:length(citcomnames)){
+    proxmat[i,j]=sum(adj[interdisc$citclass==citcomnames[i],interdisc$citclass==citcomnames[j]])/sum(adj[interdisc$citclass==citcomnames[i],])
+  }
+}
+rownames(proxmat)<-citcomnames;colnames(proxmat)<-citcomnames
+dd=melt(proxmat[,rev(colnames(proxmat))])
+dd$value=cut(dd$value,breaks = 11)
+
+g=ggplot(dd,aes(x=Var1,y=Var2,fill=value))
+g+geom_raster()+scale_fill_brewer(palette = 'Spectral',direction = -1,name='Proximity')+xlab('')+ylab('')
+ggsave(file=paste0(figdir,'citation_proximities.png'),width=21,height=16,units = 'cm')
+
+
+# semantic proximities
+
+# prox between semantic classes
+#t(subprobas[interdisc$citclass%in%citcomnames,])%*%subprobas[interdisc$citclass%in%citcomnames,]
+# no time.
+
+# tensor product to have semantic distance between cit classes
+#  not : we could have done that in patents ?
+
+docdist=matrix(0,nrow(subprobas),nrow(subprobas));rownames(docdist)<-rownames(subprobas);colnames(docdist)<-rownames(subprobas)
+for(j in 1:ncol(subprobas)){
+  docdist=docdist+(matrix(rep(subprobas[,j],nrow(subprobas)),nrow=nrow(subprobas),byrow = T) - matrix(rep(subprobas[,j],nrow(subprobas)),nrow=nrow(subprobas),byrow = F))^2
+}
+docdist=1-sqrt(docdist/2)
+
+proxmat = matrix(0,length(citcomnames),length(citcomnames))
+for(i in 1:length(citcomnames)){
+  for(j in 1:length(citcomnames)){
+    proxmat[i,j]=sum(docdist[interdisc$citclass==citcomnames[i],interdisc$citclass==citcomnames[j]])/(length(which(interdisc$citclass==citcomnames[i]))*length(which(interdisc$citclass==citcomnames[j])))
+  }
+}
+rownames(proxmat)<-citcomnames;colnames(proxmat)<-citcomnames
+
+dd=melt(proxmat[,rev(colnames(proxmat))])
+dd$value=cut(dd$value,breaks = 11)
+
+g=ggplot(dd,aes(x=Var1,y=Var2,fill=value))
+g+geom_raster()+scale_fill_brewer(palette = 'Spectral',direction = -1,name='Proximity')+xlab('')+ylab('')
+ggsave(file=paste0(figdir,'semantic_proximities.png'),width=21,height=16,units = 'cm')
+
+
+
+###
+# Correlation and modularity
+
+# coonstruct citation proba matrix
+semprobas = subprobas[interdisc$citclass%in%citcomnames,]
+
+citprobas = matrix(0,nrow(semprobas),length(citcomnames)) # do it dirty
+colnames(citprobas)<-citcomnames;rownames(citprobas)<-rownames(semprobas)
+for(i in 1:nrow(citprobas)){citprobas[i,as.character(interdisc[rownames(citprobas)[i],"citclass"])]=1}
+
+bcorrs=bootstrapped(semprobas,citprobas)
+corrs = corrMat(semprobas,citprobas)
+min(corrs);max(corrs);mean(abs(corrs))
+apply(bcorrs,2,mean)
+apply(bcorrs,2,sd)
+
+
+# overlapping modularity
+overlappingmodularity(semprobas,adj[rownames(semprobas),rownames(semprobas)])
+overlappingmodularity(citprobas,adj[rownames(citprobas),rownames(citprobas)])
+
+semmods=c();citmods=c()
+for(b in 1:100){
+  show(b)
+  shuffled=adj[sample(rownames(semprobas),size=nrow(semprobas),replace = F),sample(rownames(semprobas),size=nrow(semprobas),replace = F)];
+  semmods=append(semmods,overlappingmodularity(semprobas,shuffled))
+  citmods=append(citmods,overlappingmodularity(citprobas,shuffled))
+}
+
+show(paste0('sem : ',mean(semmods),' +- ',sd(semmods)))
+show(paste0('cit : ',mean(citmods),' +- ',sd(citmods)))
+
+
+
+
+
+
