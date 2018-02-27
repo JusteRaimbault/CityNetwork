f06b1540cf669e67c912c8782efd59f5b1daf72f
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-02-27 16:51:10 +0100

    revision_intgib

--- Docs/Papers/InteractionGibrat/paper/figures.sh
+++ Docs/Papers/InteractionGibrat/paper/figures.sh
@@ -1,19 +1,24 @@
 
 # Fig 1
-cp figuresraw/empirical_tsCorrelations.png figures/Fig1.png
+#cp figuresraw/empirical_tsCorrelations.png figures/Fig1.png
+cp figuresraw/4-3-2-fig-interactiongibrat-ts-correlations.jpg figures/Fig1.jpg
 
 # Fig 2
-montage figuresraw/exview_g0.0136_wg1.18e-4_ggamma1.87_gdecay307.1_wn0_RouenMarseille.png figuresraw/fits_g0.0136_wg1.18e-4_ggamma1.87_gdecay307.1_wn0_RouenMarseille.png -tile 2x1 -geometry +10+0 figures/Fig2.png 
+#montage figuresraw/exview_g0.0136_wg1.18e-4_ggamma1.87_gdecay307.1_wn0_RouenMarseille.png figuresraw/fits_g0.0136_wg1.18e-4_ggamma1.87_gdecay307.1_wn0_RouenMarseille.png -tile 2x1 -geometry +10+0 figures/Fig2.png
+cp figuresraw/4-3-2-fig-interactiongibrat-interface.jpg figures/Fig2.jpg
 
 # Fig 3
-montage figuresraw/logmse-feedbackDecay_ZOOM.png figuresraw/mselog-feedbackDecay_ZOOM.png -tile 2x1 -geometry +10+0 figures/Fig3.png
+#montage figuresraw/logmse-feedbackDecay_ZOOM.png figuresraw/mselog-feedbackDecay_ZOOM.png -tile 2x1 -geometry +10+0 figures/Fig3.png
+cp figuresraw/4-3-2-fig-interactiongibrat-networkeffects.jpg figures/Fig3.jpg
 
 # Fig 4
-cp figuresraw/gravity.png figures/Fig4.png
+#cp figuresraw/gravity.png figures/Fig4.png
+cp figuresraw/4-3-2-fig-interactiongibrat-gravity-pareto.jpg figures/Fig4.jpg
 
 # Fig 5
-montage figuresraw/growthRate_filt1.png figuresraw/gravityWeight_filt1.png figuresraw/gravityDecay_filt1.png figuresraw/gravityGamma_filt1.png -tile 2x2 -geometry +10+10 figures/Fig5.png
+#montage figuresraw/growthRate_filt1.png figuresraw/gravityWeight_filt1.png figuresraw/gravityDecay_filt1.png figuresraw/gravityGamma_filt1.png -tile 2x2 -geometry +10+10 figures/Fig5.png
+cp figuresraw/4-3-2-fig-interactiongibrat-gravity-params.jpg figures/Fig5.jpg
 
 # Fig 6
-montage figuresraw/growthRate_filt0.png figuresraw/gravityWeight_relativegrowthRate.png figuresraw/feedbackDecay_filt0.png figuresraw/feedbackGamma_filt0.png -tile 2x2 -geometry +10+10 figures/Fig6.png
-
+#montage figuresraw/growthRate_filt0.png figuresraw/gravityWeight_relativegrowthRate.png figuresraw/feedbackDecay_filt0.png figuresraw/feedbackGamma_filt0.png -tile 2x2 -geometry +10+10 figures/Fig6.png
+cp figuresraw/4-3-2-fig-interactiongibrat-feedback.jpg figures/Fig6.jpg
