b0b4132853e7c16a2a426bbe07d8e1d5605e727c
Juste Raimbault <juste.raimbault@polytechnique.edu>
2018-05-29 10:24:35 +0200

    rebuild_kwreq

--- Models/QuantEpistemo/AlgoSR/AlgoSRJavaApp/src/main/KeywordsRequest.java
+++ Models/QuantEpistemo/AlgoSR/AlgoSRJavaApp/src/main/KeywordsRequest.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package main;
 
@@ -21,26 +21,28 @@ import utils.tor.TorPoolManager;
  */
 public class KeywordsRequest {
 
-	
-	
+
+	/**
+	*  Construct a corpus from keyword request
+	*/
 	public static void main(String[] args) {
-		
-		
+
+
 		if(args.length==3||args.length==4){
 			String kwFile=args[0];
 			String outFile=args[1];
 			int numref = Integer.parseInt(args[2]);
 			String addterm = "";if(args.length==4){addterm=args[3];}
-			
+
 			try{TorPoolManager.setupTorPoolConnexion();}catch(Exception e){e.printStackTrace();}
 			ScholarAPI.init();
-			
+
 			// existing corpus - not needed, will go through the keywords anyway
 			/*Corpus existing = new DefaultCorpus();
 			if(new File(outFile).exists()){
 				existing = new CSVFactory(outFile).getCorpus();
 			}*/
-			
+
 			// parse kws file
 			String[][] kwraw = CSVReader.read(kwFile, ",","\"");
 			String[] reqs = new String[kwraw.length];
@@ -49,24 +51,24 @@ public class KeywordsRequest {
 				if(addterm.length()>0){currentreq=currentreq+"+"+addterm;}
 			    reqs[i] = currentreq ;
 			}
-			
+
 			try{(new FileWriter(new File(outFile+"_achieved.txt"))).write(kwFile+'\n');}catch(Exception e){e.printStackTrace();}
-			
+
 			for(String req:reqs){
 				HashSet<Reference> currentrefs = ScholarAPI.scholarRequest(req, numref, "direct");
 				new DefaultCorpus(Reference.references.keySet()).csvExport(outFile,false);
 				// write kws in achieved file
 				try{(new FileWriter(new File(outFile+"_achieved.txt"),true)).write(req+"\n");}catch(Exception e){e.printStackTrace();}
 			}
-			
-			
+
+
 		}else{
 			System.out.println("usage : java -jar keywordsRequest.jar kwfile outfile numrefs [addterm]");
 		}
-		
-		
-		
-		
+
+
+
+
 	}
-	
+
 }
